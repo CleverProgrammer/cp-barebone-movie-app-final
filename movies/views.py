785e0277ce2bf1ce9f533a72582e37de07db8743
cleverqazi <qazi+pp@cleverprogrammer.com>
2018-01-14 11:13:04 +0200

    Ability to pull movies from Airtable

---
+++ movies/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from airtable import Airtable
+import os
+
+
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+              'Movies',
+              api_key=os.environ.get('AIRTABLE_API_KEY'))
+
+# Create your views here.
+def home_page(request):
+    user_query = str(request.GET.get('query', ''))
+    search_result = AT.get_all(formula="FIND('" + user_query + "', {Name})")
+    stuff_for_frontend = {"search_result": search_result}
+    return render(request, "movies/movies_stuff.html", stuff_for_frontend)
