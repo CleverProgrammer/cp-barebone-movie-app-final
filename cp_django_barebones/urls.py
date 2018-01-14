5949e7271964cd9e4806556ccba138d2883ae86d
cleverqazi <qazi+pp@cleverprogrammer.com>
2018-01-14 09:23:54 +0000

    Getting this project prepped up for CP students as a barebone python django fullstack app. This has all the front end they need.

--- cp_django_barebones/urls.py
+++ cp_django_barebones/urls.py
@@ -17,7 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('movies.urls')),
-    path('movies/', include('movies.urls')),
     path('admin/', admin.site.urls),
 ]
--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
 
-
-urlpatterns = [
-    path('', views.home_page, name='home_page'),
-]
