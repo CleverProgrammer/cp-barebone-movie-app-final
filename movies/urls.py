8e77a8fbcf7f55ce9c7221087ff1012d23e594d0
cleverqazi <qazi+pp@cleverprogrammer.com>
2018-01-20 08:25:43 +0000

    Add frontend for create new movie button and modal.

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('create/', views.create, name='create')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from airtable import Airtable
 import os
@@ -14,3 +14,7 @@ def home_page(request):
     search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
     stuff_for_frontend = {'search_result': search_result}
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+
+
+def create(request):
+    return redirect('/')
